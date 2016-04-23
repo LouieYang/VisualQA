4e3a6bf575cc3142471cb3b228dac5c77ce3b0ed
Louie <651636074@qq.com>
2016-04-23 20:08:04 +0800

    Add cpp file

---
+++ Cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "vqa_system.hpp"
+#include "convert_data.hpp"
+
+int main()
+{
+//    convert_lmdb();
+//    convert_lmdb("question_vector.csv","VAQTrain2014/","answers.csv",
+//                 "key_mapping.txt","test.lmdb", ConvertMode::WRITE);
+
+//    convert_lmdb("sample_question.csv", "VAQTrain2014/", "sample_answers.csv", "key_mapping_sample.txt", "sample_vqa_lmdb", ConvertMode::WRITE);
+    VQASystem vqas("googlenet_deploy.prototxt", "googlenet.caffemodel",
+                   "softmax_deploy.prototxt", "VQA_train_iter_300000.caffemodel");
+    
+    cv::Mat img = cv::imread("demo.jpg");
+    std::vector<std::string> res(vqas.getTopNAnswer(img, "demo.csv", 3));
+    
+    
+    std::copy(res.begin(), res.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+    
+}
